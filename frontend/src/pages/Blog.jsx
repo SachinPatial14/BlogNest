32f3bd15cd9d15017c1764160cb4e14c267c2e33
Sachin <patialsachin2004@gmail.com>
2025-10-30 11:39:20 +0530

    Enhance Blog component with comments functionality, add Loader component, and integrate Footer

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, blog_data } from '../assets/assets';
+import { assets, blog_data, comments_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
 import Moment from 'moment'
+import Footer from '../components/Footer';
 
 const Blog = () => {
   const [data,setData] = useState(null);
+  const [comment,setComment] = useState([]);
+  const [name,setName] = useState("");
+  const [content,setContent] = useState("")
   const {id} = useParams();
 
   const fetchBlogData = async()=>{
@@ -13,20 +17,70 @@ const Blog = () => {
     setData(data) ;
   };
 
+  const fetchComments = ()=>{
+    setComment(comments_data)
+  };
+
+  const addComment =async(e)=>{
+       e.preventDefault();
+  }
+
   useEffect(()=>{
-    fetchBlogData()
+    fetchBlogData();
+    fetchComments();
   },[]);
 
   return data ? (
     <div className='relative'>
       <img src={assets.gradientBackground} alt='' className='absolute -top-50 -z-1 opacity-50'/>
       <Navbar />
-     <div>
-        <p>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+     <div className='text-center mt-20 text-gray-600'>
+        <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+        <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
      </div>
-     <div>
+     <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
+         <img src={data.image} alt="image" className='rounded-3xl mb-5' />
+         <div className='rich-text max-w-3xl max-auto' dangerouslySetInnerHTML={{__html:data.description}}></div>
+         {/* comment section */}
+         <div className='mt-14 mb-10 max-w'>
+           <p className='font-semibold mb-4'>Comments ({comment.length})</p>
+           <div className='flex flex-col gap-4'>
+              {comment.map((item,index)=>(
+                <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'>
+                  <div className='flex items-center gap-2 mb-2'>
+                     <img src={assets.user_icon} alt="user icon" className='w-6' />
+                     <p className='font-medium'>{item.name}</p>
+                  </div>
+                   <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                   <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+                </div>
+              ))}
+           </div>
+         </div>
+         {/* add comment section */}
 
+         <div className='max-w-3xl mx-auto'>
+          <p className='font-semibold mb-4'>Add your comment</p>
+          <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+             <input onChange={(e)=>setName(e.target.value)} value={name} type='text' placeholder='Enter your name' required className='w-full p-2 border border-gray-300 rounded outline-none' />
+             <textarea onChange={(e)=>setContent(e.target.value)} value={content} placeholder='Enter comment' className='w-full p-2 border border-gray-300 rounded outine-none h-48' required></textarea>
+             <button type='submit' className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'>Submit</button>
+          </form>
+         </div>
+         {/* share buttons */}
+
+         <div className='my-24 max-w-3xl mx-auto'>
+          <p className='font-semibold my-4'>Share this article on Social media</p>
+          <div className='flex'>
+            <img  src={assets.facebook_icon} width={50} alt='facebook icon'/>
+            <img src={assets.twitter_icon} width={50} alt="twitter icon" />
+            <img src={assets.googleplus_icon} width={50} alt="google-plus icon" />
+          </div>
+         </div>
      </div>
+     <Footer />
     </div>
   ): <div>
     Loading ...
